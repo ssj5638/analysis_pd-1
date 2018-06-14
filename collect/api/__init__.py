4783f069a348e1c54d9e83e75da2e123dcc7bc58
bit <kickscar@gmail.com>
2018-06-14 13:45:41 +0900

    init commit

---
+++ collect/api/__init__.py
@@ -0,0 +1,4 @@
+from .api import pd_gen_url
+from .api import pd_fetch_foreign_visitor
+from .api import pd_fetch_tourspot_visitor
+
--- collect/api/api.py
+++ collect/api/api.py
@@ -1,4 +1,44 @@
 # PD API Wrapper Functions
+from datetime import datetime
 from urllib.parse import urlencode
 from .web_request import json_request
 
+SERVICE_KEY = '%2FfZdR%2Bue1CSxLEnMkZXa9iDYontLTMTIteD5%2BzYCiMYpDKUZNUh2FHGDQ04zazSEmLl34FClDQk8a7flFCIQKA%3D%3D'
+
+
+def pd_gen_url(endpoint, **param):
+    url = '%s?%s&serviceKey=%s' % (endpoint, urlencode(param), SERVICE_KEY)
+    return url
+
+
+def pd_fetch_foreign_visitor(country_code, year, month):
+    endpoint = 'http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList'
+    url = pd_gen_url(
+        endpoint,
+        YM='{0:04d}{1:02d}'.format(year, month),
+        NAT_CD=country_code,
+        ED_CD='E',
+        _type='json')
+    json_result = json_request(url=url)
+
+    json_response = json_result.get('response')
+    json_header = json_response.get('header')
+    result_message = json_header.get('resultMsg')
+    if 'OK' != result_message:
+        print('%s Error[%s] for request %s' % (datetime.now(), result_message, url))
+        return None
+
+    json_body = json_response.get('body')
+    json_items = json_body.get('items')
+
+    return json_items.get('item') if isinstance(json_items, dict) else None
+
+
+
+def pd_fetch_tourspot_visitor(
+        district1='',
+        district2='',
+        tourspot='',
+        year=0,
+        month=0):
+    pass
